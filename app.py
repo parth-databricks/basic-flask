1c5e59f29bf55edaf9ad2024347ec1d0a7eab9ba
Parth Sundarka <parth.sundarka@databricks.com>
2024-02-02 10:09:39 +0530

    Init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/name/<name>')
+def hello_world2(name):
+    return json.dumps({"name" : name})
+    #return 'Hello, Name!' + name
+
+
+if __name__ == "__main__":
+    app.run()
